b28e249b868ba612d2dab350eae12893f9fcd8f5
Michela <micheladellagatta1@gmail.com>
2026-02-11 18:00:44 +0100

    style: improve movie card layout and styling

--- resources/views/components/cards.blade.php
+++ resources/views/components/cards.blade.php
@@ -1,11 +1,24 @@
-<div class="card h-100">
-  <div class="card-body">
-    <h5 class="card-title">{{$movieTitle}}</h5>
-    <p class="card-text">{{$movieOriginalTitle}}</p>
+<div class="card h-100 shadow-sm border-0">
+  <div class="card-body d-flex flex-column">
+    <div class="flex-grow-1">
+      <h4 class="card-title mb-1 fw-bold">{{ $movieTitle }}</h4>
+      <p class="card-text small text-muted mb-3 italic">
+        ({{ $movieOriginalTitle }})
+      </p>
+      <div class="d-flex flex-column gap-2 mb-3">
+        <span class="card-text small text-muted mb-3 italic">
+          {{ $movieNationality }}
+        </span>
+        <span class="small text-secondary">
+          {{ $movieDate }}
+        </span>
+      </div>
+    </div>
+
+    <div class="mt-2">
+      <div class="p-2 rounded-bottom text-end" style="background-color: #e2eeff;">
+          <span class="text-dark small"><span class="fw-bold fs-5">{{ $movieVote }}</span>/10</span>
+      </div>
+    </div>
   </div>
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">{{$movieNationality}}</li>
-    <li class="list-group-item">{{$movieDate}}</li>
-    <li class="list-group-item">{{$movieVote}}/10</li>
-  </ul>
 </div>
